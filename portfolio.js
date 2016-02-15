aac191b746dab16ff7b43375bc5851d6c30307aa
Becky Beauchamp <beckybeauchamp1@gmail.com>
2016-02-15 14:57:59 -0500

    Adds inline comments

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,6 @@
+//Looks you tried to start jQuery here, I would hold off on continuing with jQuery until after learning Vanilla
+// js first
+
 $( document ).ready(function() {
   $('.menu-item').
 });
